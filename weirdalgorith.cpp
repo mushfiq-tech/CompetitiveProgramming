88f4dfc3b6b13240435232217947a5a0e488bd08
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-25 22:32:46 +0600

    1

--- weirdalgorith.cpp
+++ weirdalgorith.cpp
@@ -5,6 +5,18 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
+    cin>> n;
+
+    if (n%2 == 0)
+    {
+        n = n/2;
+    }
+    else if (n%2 != 0)
+    {
+        n = n*3+1;
+    }
+
+    cout<<n;
     
 
     return 0;
