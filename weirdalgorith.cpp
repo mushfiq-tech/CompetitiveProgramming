0b0cab877e1db0560defaca4ab3b71898c738a11
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-25 22:54:50 +0600

    1

--- weirdalgorith.cpp
+++ weirdalgorith.cpp
@@ -10,13 +10,25 @@ int main(){
     if (n%2 == 0)
     {
         n = n/2;
+        cout<<n;
     }
     else if (n%2 != 0)
     {
         n = n*3+1;
+        cout<<n;
     }
 
-    cout<<n;
+    else if (n == 1)
+    {
+        cout<<n;
+    }
+    else if (n>1 && n%2 == 0)
+    {
+        n = n/2;
+        cout<<n;
+    }
+    
+
     
 
     return 0;
