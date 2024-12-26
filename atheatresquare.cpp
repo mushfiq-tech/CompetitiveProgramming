9f7121bd7283d5294028ae59f72438ded7a88ecd
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-26 09:48:21 +0600

    1

--- atheatresquare.cpp
+++ atheatresquare.cpp
@@ -7,20 +7,20 @@ int main(){
 
     int n,m,a;
     cin>>n>>m>>a;
-    int r=n/a;
-    int p=m/a;
+    int r = ceil(double(n)/double(a));
+    int p = ceil(double(m)/double(a));
     int k,l,s;
+    k = 0;
+    l= 0;
 
-
-    for(int i=0; i=r; i++){
-        k=k+1;
-        for (int j = 0; j = p; j++)
-        {
-            l=l+1;
-        }
-        
-        
+    for(int i=0; i<r; i++){
+        k++;  
     }
+
+    for (int i = 0; i < p; i++)
+        {
+            l++;
+        }
     s=k*l;
     cout<<s;
     return 0;
