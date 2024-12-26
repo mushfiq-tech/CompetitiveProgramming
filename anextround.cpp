5c88176a51cdae175ccf921261a4afda834370b7
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-26 23:25:56 +0600

    1.0

--- anextround.cpp
+++ anextround.cpp
@@ -6,8 +6,18 @@ int main(){
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
 
+        int k,n,t;
+        cin>> k >> n ;
+        vector<int>a(n);
         
+        for(int i=0;i<n;i++)
+            {
+                cin>>a[i];
+                t=i+(a[i]>=k);
+                
+            }
 
+        cout<<t;
 
 
     return 0;
