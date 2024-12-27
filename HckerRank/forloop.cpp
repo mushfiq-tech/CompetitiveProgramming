a80d1da20407f26756418878ebf7c220424f8ddd
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-27 18:39:31 +0600

    1.1

--- HckerRank/forloop.cpp
+++ HckerRank/forloop.cpp
@@ -6,6 +6,25 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    string numbers[11]={"one","two","three","four","five","six","seven","eight","nine"};
+
+    for (int i = a; i<= b; i++)
+    {
+        if (i)
+        {
+            /* code */
+        }
+        
+    }
+    
+
+    
+
 
 
 
