683fe5366edab4e3216957be222b0b9f77630115
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-28 15:05:16 +0600

    1.0

---
+++ CpsAcademy/audiblerange.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int T , X ;
+    cin>> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin>>X;
+        if (X>=67 && X<=4500)
+        {
+            cout<<"YES\n";
+        }
+
+        else
+        {
+            cout<<"NO\n";
+        }
+        
+        
+    }
+    
+
+
+
+
+    return 0;
+}
