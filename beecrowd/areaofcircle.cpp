c6d8fcd2fd72258c596beb05ed7c23131a3b0666
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-29 21:12:28 +0600

    1.0

---
+++ beecrowd/areaofcircle.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    double A;
+    double R = 0;
+    double p;
+    cin>>R;
+    p = 3.14159;
+    A= p*(R*R);
+    
+    cout<<fixed<<setprecision(4);
+    cout<<"A="<<A<<endl;
+ 
+    return 0;
+}
