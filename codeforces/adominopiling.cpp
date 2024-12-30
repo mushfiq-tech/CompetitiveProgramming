a7b766e9890ee06d2d65d3147d6d0de178e6e842
Mushfiq <mushfiq.rahm@gmail.com>
2024-12-30 12:56:37 +0600

    1

---
+++ codeforces/adominopiling.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int M,N;
+    int A;
+    int count;
+    count = 0;
+    cin>>M>>N;
+    A= M*N;
+    count = A/2;
+    
+    cout<<count<<endl;
+    return 0;
+}
