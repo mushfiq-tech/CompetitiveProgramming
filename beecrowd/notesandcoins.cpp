88511182add5dc2ea0dc7f4440bc0dc09250ff3c
Mushfiq <mushfiq.rahm@gmail.com>
2025-01-04 00:00:20 +0600

    1

--- beecrowd/notesandcoins.cpp
+++ beecrowd/notesandcoins.cpp
@@ -9,6 +9,9 @@ int main()
     double input;
     cin>>input;
 
-    double nac[] = {};
+    double notes[] = {100,50,20,10,5,2};
+    double coins[]= {1.00,0.50,0.20,0.10,0.05,0.02,0.01};
+
+    
     return 0;
 }
